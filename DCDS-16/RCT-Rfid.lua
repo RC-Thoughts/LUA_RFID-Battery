69d91ceb579396d642d986804fe4b9b658fd8605
Pertti Erkkilä <pertti.erkkila@gmail.com>
2017-02-10 11:35:18 +0200

    Optimization fixes

--- DCDS-16/RCT-Rfid.lua
+++ DCDS-16/RCT-Rfid.lua
@@ -19,23 +19,28 @@
 	Released under MIT-license by Tero @ RC-Thoughts.com 2017
 	---------------------------------------------------------
 --]]
+collectgarbage()
+
 ----------------------------------------------------------------------
 -- Locals for the application
+local rfidVersion, tCurRFID, tStrRFID = "1.9", 0, 0
 local rfidId, rfidParam, rfidSens, mahId, mahParam, mahSens
-local capaAlarm, capaAlarmTr, alarmVoice, vPlayed, tagId
-local rfidTime, annGo, annSw, tagCapa
+local capaAlarm, capaAlarmTr, alarmVoice, vPlayed, tagID
+local rfidTime, annGo, annSw, tagCapa, alarm1Tr
 local tagValid, tSetAlm, percVal, annTime = 0, 0, "-", 0
 local sensorLa1list = { "..." }
 local sensorId1list = { "..." }
 local sensorPa1list = { "..." }
+local trans8
 ----------------------------------------------------------------------
 -- Function for translation file-reading
 local function readFile(path)
     local f = io.open(path, "r")
-    local lines = {}
     if (f) then
+        local read = io.read
+        local lines = {}
         while 1 do
-            local buf = io.read(f, 512)
+            local buf = read(f, 512)
             if (buf ~= "") then
                 lines[#lines + 1] = buf
             else
@@ -50,40 +55,50 @@ end
 ----------------------------------------------------------------------
 -- Read translations
 local function setLanguage()
-    local lng = system.getLocale();
     local file = readFile("Apps/Lang/RCT-Rfid.jsn")
     local obj = json.decode(file)
     if (obj) then
+        local lng = system.getLocale();
         trans8 = obj[lng] or obj[obj.default]
     end
 end
 
 ----------------------------------------------------------------------
 -- Read available sensors for user to select
-local sensors = system.getSensors()
-for i, sensor in ipairs(sensors) do
-    if (sensor.label ~= "") then
-        table.insert(sensorLa1list, string.format("%s", sensor.label))
-        table.insert(sensorId1list, string.format("%s", sensor.id))
-        table.insert(sensorPa1list, string.format("%s", sensor.param))
+local function readSensors()
+    local sensors = system.getSensors()
+    local format = string.format
+    local insert = table.insert
+    for i, sensor in ipairs(sensors) do
+        if (sensor.label ~= "") then
+            insert(sensorLa1list, format("%s", sensor.label))
+            insert(sensorId1list, format("%s", sensor.id))
+            insert(sensorPa1list, format("%s", sensor.param))
+        end
     end
 end
+
 ----------------------------------------------------------------------
 -- Draw the telemetry windows
 local function printBattery()
+    local lcd = lcd
+    local drawText = lcd.drawText
+    local getTextWidth = lcd.getTextWidth
+    local bold = FONT_BOLD
+
     if (tagID == 0) then
-        lcd.drawText((150 - lcd.getTextWidth(FONT_BOLD, trans8.emptyTag)) / 2, 24, trans8.emptyTag, FONT_BOLD)
+        drawText((150 - getTextWidth(bold, trans8.emptyTag)) / 2, 24, trans8.emptyTag, bold)
     elseif (mahId == 0) then
-        lcd.drawText((150 - lcd.getTextWidth(FONT_BOLD, trans8.noCurr)) / 2, 24, trans8.noCurr, FONT_BOLD)
+        drawText((150 - getTextWidth(bold, trans8.noCurr)) / 2, 24, trans8.noCurr, bold)
     elseif (percVal ~= "-") then
         lcd.drawRectangle(6, 9, 26, 55)
         lcd.drawFilledRectangle(13, 6, 12, 4)
-        chgY = (65 - (percVal * 0.54))
-        chgH = ((percVal * 0.54) - 1)
+        local chgY = (65 - (percVal * 0.54))
+        local chgH = ((percVal * 0.54) - 1)
         lcd.drawFilledRectangle(7, chgY, 24, chgH)
-        lcd.drawText(148 - lcd.getTextWidth(FONT_MAXI, string.format("%.1f%%", percVal)), 15, string.format("%.1f%%", percVal), FONT_MAXI)
+        drawText(148 - getTextWidth(FONT_MAXI, string.format("%.1f%%", percVal)), 15, string.format("%.1f%%", percVal), FONT_MAXI)
     else
-        lcd.drawText((150 - lcd.getTextWidth(FONT_BOLD, trans8.noPack)) / 2, 24, trans8.noPack, FONT_BOLD)
+        drawText((150 - getTextWidth(bold, trans8.noPack)) / 2, 24, trans8.noPack, bold)
     end
 end
 
@@ -91,10 +106,13 @@ end
 -- Store settings when changed by user
 --
 local function capaAlarmChanged(value)
+    local pSave = system.pSave
+
     capaAlarm = value
-    system.pSave("capaAlarm", value)
+    pSave("capaAlarm", value)
+
     alarm1Tr = string.format("%.1f", capaAlarm)
-    system.pSave("capaAlarmTr", capaAlarmTr)
+    pSave("capaAlarmTr", capaAlarmTr)
     system.registerTelemetry(1, trans8.telLabel, 2, printBattery)
 end
 
@@ -105,29 +123,35 @@ end
 
 --
 local function sensorIDChanged(value)
+    local pSave = system.pSave
+    local format = string.format
+
     rfidSens = value
-    system.pSave("rfidSens", value)
-    rfidId = string.format("%s", sensorId1list[rfidSens])
-    rfidParam = string.format("%s", sensorPa1list[rfidSens])
+    pSave("rfidSens", value)
+    rfidId = format("%s", sensorId1list[rfidSens])
+    rfidParam = format("%s", sensorPa1list[rfidSens])
     if (rfidId == "...") then
         rfidId = 0
         rfidParam = 0
     end
-    system.pSave("rfidId", rfidId)
-    system.pSave("rfidParam", rfidParam)
+    pSave("rfidId", rfidId)
+    pSave("rfidParam", rfidParam)
 end
 
 local function sensorMahChanged(value)
+    local pSave = system.pSave
+    local format = string.format
+
     mahSens = value
-    system.pSave("mahSens", value)
-    mahId = string.format("%s", sensorId1list[mahSens])
-    mahParam = string.format("%s", sensorPa1list[mahSens])
+    pSave("mahSens", value)
+    mahId = format("%s", sensorId1list[mahSens])
+    mahParam = format("%s", sensorPa1list[mahSens])
     if (mahId == "...") then
         mahId = 0
         mahParam = 0
     end
-    system.pSave("mahId", mahId)
-    system.pSave("mahParam", mahParam)
+    pSave("mahId", mahId)
+    pSave("mahParam", mahParam)
 end
 
 local function annSwChanged(value)
@@ -137,46 +161,51 @@ end
 
 ----------------------------------------------------------------------
 -- Draw the main form (Application inteface)
-local function initForm(subform)
+local function initForm()
+    local form = form
+    local addRow = form.addRow
+    local addLabel = form.addLabel
+
     form.setButton(1, ":tools")
 
-    form.addRow(1)
-    form.addLabel({ label = "---   RC-Thoughts Jeti Tools    ---", font = FONT_BIG })
+    addRow(1)
+    addLabel({ label = "---   RC-Thoughts Jeti Tools    ---", font = FONT_BIG })
 
-    form.addRow(1)
-    form.addLabel({ label = trans8.labelCommon, font = FONT_BOLD })
+    addRow(1)
+    addLabel({ label = trans8.labelCommon, font = FONT_BOLD })
 
-    form.addRow(2)
-    form.addLabel({ label = trans8.sensorID })
+    addRow(2)
+    addLabel({ label = trans8.sensorID })
     form.addSelectbox(sensorLa1list, rfidSens, true, sensorIDChanged)
 
-    form.addRow(2)
-    form.addLabel({ label = trans8.sensorMah })
+    addRow(2)
+    addLabel({ label = trans8.sensorMah })
     form.addSelectbox(sensorLa1list, mahSens, true, sensorMahChanged)
 
-    form.addRow(1)
-    form.addLabel({ label = trans8.labelAlarm, font = FONT_BOLD })
+    addRow(1)
+    addLabel({ label = trans8.labelAlarm, font = FONT_BOLD })
 
-    form.addRow(2)
-    form.addLabel({ label = trans8.AlmVal })
+    addRow(2)
+    addLabel({ label = trans8.AlmVal })
     form.addIntbox(capaAlarm, 0, 100, 0, 0, 1, capaAlarmChanged)
 
-    form.addRow(2)
-    form.addLabel({ label = trans8.selAudio })
+    addRow(2)
+    addLabel({ label = trans8.selAudio })
     form.addAudioFilebox(alarmVoice, alarmVoiceChanged)
 
-    form.addRow(2)
-    form.addLabel({ label = trans8.annSw, width = 220 })
+    addRow(2)
+    addLabel({ label = trans8.annSw, width = 220 })
     form.addInputbox(annSw, true, annSwChanged)
 
-    form.addRow(1)
-    form.addLabel({ label = "Powered by RC-Thoughts.com - v." .. rfidVersion .. " ", font = FONT_MINI, alignRight = true })
+    addRow(1)
+    addLabel({ label = "Powered by RC-Thoughts.com - v." .. rfidVersion .. " ", font = FONT_MINI, alignRight = true })
 
     form.setFocusedRow(1)
 end
 
 ----------------------------------------------------------------------
 local function loop()
+    local system = system
     -- RFID reading and battery-definition
     if (rfidSens > 1) then
         rfidTime = system.getTime()
@@ -193,10 +222,9 @@ local function loop()
         end
         -- Capacity percentage calculation and voice alert config
         if (mahSens > 1) then
-            mahCapa = system.getSensorByID(mahId, mahParam)
+            local mahCapa = system.getSensorByID(mahId, mahParam)
             if (mahCapa and mahCapa.valid) then
                 mahCapa = mahCapa.value
-                mahCapaLog = mahCapa
                 if (tagValid == 1) then
                     if (tSetAlm == 0) then
                         tCurRFID = rfidTime
@@ -205,7 +233,7 @@ local function loop()
                     else
                         tCurRFID = system.getTime()
                     end
-                    resRFID = (((tagCapa - mahCapa) * 100) / tagCapa)
+                    local resRFID = (((tagCapa - mahCapa) * 100) / tagCapa)
                     if (resRFID < 0) then
                         resRFID = 0
                     else
@@ -248,21 +276,26 @@ end
 ----------------------------------------------------------------------
 -- Application initialization
 local function init()
-    rfidId = system.pLoad("rfidId", 0)
-    rfidParam = system.pLoad("rfidParam", 0)
-    rfidSens = system.pLoad("rfidSens", 0)
-    mahId = system.pLoad("mahId", 0)
-    mahParam = system.pLoad("mahParam", 0)
-    mahSens = system.pLoad("mahSens", 0)
-    capaAlarm = system.pLoad("capaAlarm", 0)
-    capaAlarmTr = system.pLoad("capaAlarmTr", 1)
-    alarmVoice = system.pLoad("alarmVoice", "...")
-    annSw = system.pLoad("annSw")
+    local pLoad = system.pLoad
+    rfidId = pLoad("rfidId", 0)
+    rfidParam = pLoad("rfidParam", 0)
+    rfidSens = pLoad("rfidSens", 0)
+    mahId = pLoad("mahId", 0)
+    mahParam = pLoad("mahParam", 0)
+    mahSens = pLoad("mahSens", 0)
+    capaAlarm = pLoad("capaAlarm", 0)
+    capaAlarmTr = pLoad("capaAlarmTr", 1)
+    alarmVoice = pLoad("alarmVoice", "...")
+    annSw = pLoad("annSw")
+
+    readSensors()
+
     system.registerForm(1, MENU_APPS, trans8.appName, initForm, keyPressed)
     system.registerTelemetry(1, "RFID-Battery", 2, printBattery)
+    collectgarbage()
 end
 
 ----------------------------------------------------------------------
-rfidVersion = "1.8"
 setLanguage()
+collectgarbage()
 return { init = init, loop = loop, author = "RC-Thoughts", version = rfidVersion, name = trans8.appName }
